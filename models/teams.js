27efc98f113512ef12578abcd2d606f63f56b8b5
Alex Muresan <andrei.muresan@fortech.ro>
2019-06-03 21:29:37 +0300

    Fixed issues

--- models/teams.js
+++ models/teams.js
@@ -4,13 +4,27 @@ const schema = Joi.object().keys( {
     name: Joi.string().required(),
     countryId: Joi.string().required(),
     leagueId: Joi.string().required(),
-    captainId: Joi.string().optional(),
-    shortFKId: Joi.string().optional(),
-    longFKId: Joi.string().optional(),
-    leftCornerId: Joi.string().optional(),
-    rightCornerId: Joi.string().optional(),
-    penaltiesId: Joi.string().optional(),
-    rivalTeamId: Joi.string().required(),
+    captainId: Joi.string()
+        .allow( "" )
+        .optional(),
+    shortFKId: Joi.string()
+        .allow( "" )
+        .optional(),
+    longFKId: Joi.string()
+        .allow( "" )
+        .optional(),
+    leftCornerId: Joi.string()
+        .allow( "" )
+        .optional(),
+    rightCornerId: Joi.string()
+        .allow( "" )
+        .optional(),
+    penaltiesId: Joi.string()
+        .allow( "" )
+        .optional(),
+    rivalTeamId: Joi.string()
+        .allow( "" )
+        .optional(),
     stadium: Joi.string(),
     coach: Joi.string(),
     firstColor: Joi.string(),
